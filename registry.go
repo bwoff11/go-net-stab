1066ca2ebbcd2657213ef451b38e7d5f8d08b186
Brandon <brandon.wofford@unity3d.com>
2022-11-22 22:34:41 -0800

    Update registry.go
    Matching outstanding to received working

--- registry.go
+++ registry.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"time"
 
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
@@ -56,4 +57,15 @@ func startResponseHandler() {
 func handleEchoReply(message *icmp.Message, host *net.IPAddr) {
 	body := message.Body.(*icmp.Echo)
 	log.Printf("Received response from %s with id %d and sequence %d", host, body.ID, body.Seq)
+
+	// Search outstanding pings for a match
+	for i, ping := range outstandingPings {
+		if ping.PingerID == body.ID && ping.Sequence == body.Seq {
+			receivedAt := time.Now()
+			ping.ReceivedAt = &receivedAt
+			outstandingPings = append(outstandingPings[:i], outstandingPings[i+1:]...)
+			log.Println("Matched! ID:", ping.PingerID, "Sequence:", ping.Sequence)
+			break
+		}
+	}
 }
