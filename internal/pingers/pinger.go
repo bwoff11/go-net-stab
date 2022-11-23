bdd5268ebc4f9aa946b39bc711d38edb8d2e51ea
Brandon <brandon.wofford@unity3d.com>
2022-11-23 04:42:11 -0800

    Fixed issue with starting pingers

---
+++ internal/pingers/pinger.go
@@ -0,0 +1,74 @@
+package pingers
+
+import (
+	"log"
+	"net"
+	"time"
+
+	"github.com/bwoff11/go-net-stab/internal/config"
+	"github.com/bwoff11/go-net-stab/internal/registry"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
+
+type Pinger struct {
+	ID            int
+	SourceIP      string
+	DestinationIP string
+	Sequence      int
+	Connection    *icmp.PacketConn
+}
+
+func (p *Pinger) Start() {
+	conn, err := icmp.ListenPacket("ip4:icmp", "192.168.1.11")
+	if err != nil {
+		log.Fatal("Error creating connection:", err)
+	}
+	p.Connection = conn
+
+	log.Println("Starting pinger", p.ID, "for", p.DestinationIP)
+
+	for {
+		payload := p.CreatePayload()
+		p.SendPing(payload)
+		p.Sequence++
+		time.Sleep(time.Duration(config.Config.Interval) * time.Second)
+	}
+}
+
+func (p *Pinger) CreatePayload() []byte {
+	msg := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID:   p.ID,
+			Seq:  p.Sequence,
+			Data: []byte("We've been trying to reach you about your car's extended warranty"),
+		},
+	}
+	if bytes, err := msg.Marshal(nil); err != nil {
+		log.Fatalln("Error marshalling payload:", err)
+		return nil
+	} else {
+		return bytes
+	}
+}
+
+func (p *Pinger) SendPing(payload []byte) {
+	newPing := registry.Ping{
+		PingerID:      p.ID,
+		SourceIP:      p.SourceIP,
+		DestinationIP: p.DestinationIP,
+		Sequence:      p.Sequence,
+		SentAt:        time.Now(),
+		ReceivedAt:    nil,
+	}
+	if _, err := p.Connection.WriteTo(
+		payload,
+		&net.IPAddr{
+			IP: net.ParseIP(p.DestinationIP),
+		}); err != nil {
+		log.Println("Error sending ping:", err)
+	}
+	log.Println("Sent ping", newPing.Sequence, "from", newPing.SourceIP, "to", newPing.DestinationIP)
+	registry.SentPings <- newPing
+}
--- internal/pingers/pingers.go
+++ internal/pingers/pingers.go
@@ -2,93 +2,38 @@ package pingers
 
 import (
 	"log"
-	"net"
-	"time"
 
 	"github.com/bwoff11/go-net-stab/internal/config"
-	"github.com/bwoff11/go-net-stab/internal/registry"
-	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv4"
 )
-
-type Pinger struct {
-	ID            int
-	SourceIP      string
-	DestinationIP string
-	Sequence      int
-	Connection    *icmp.PacketConn
-}
 
 var pingers []Pinger
 
 func Start() error {
 	createPingers()
-	for _, pinger := range pingers {
-		go pinger.Start()
-	}
-	log.Println("All pingers successfully created and started")
+	startPingers()
 	return nil
 }
 
 func createPingers() {
+	endpoints := config.Config.Endpoints
+	log.Println("Creating pingers for", len(endpoints), "endpoints")
+
 	var id int
-	for _, endpoint := range config.Config.Endpoints {
-		pingers = append(pingers, Pinger{
+	for _, endpoint := range endpoints {
+		newPinger := Pinger{
 			ID:            id,
 			SourceIP:      "192.168.1.11",
 			DestinationIP: endpoint,
-		})
-		log.Println("Created new pinger for", endpoint, "with ID", id)
+			Sequence:      0,
+		}
+		pingers = append(pingers, newPinger)
+		log.Println("Created pinger", newPinger.ID, "for", newPinger.DestinationIP)
 		id++
 	}
 }
 
-func (p *Pinger) Start() {
-	conn, err := icmp.ListenPacket("ip4:icmp", "192.168.1.11")
-	if err != nil {
-		panic(err)
+func startPingers() {
+	for i := range pingers {
+		go pingers[i].Start()
 	}
-	p.Connection = conn
-
-	for {
-		payload := p.CreatePayload()
-		p.SendPing(payload)
-		p.Sequence++
-		time.Sleep(time.Duration(config.Config.Interval) * time.Second)
-	}
-}
-
-func (p *Pinger) CreatePayload() []byte {
-	msg := icmp.Message{
-		Type: ipv4.ICMPTypeEcho, Code: 0,
-		Body: &icmp.Echo{
-			ID:   p.ID,
-			Seq:  p.Sequence,
-			Data: []byte("We've been trying to reach you about your car's extended warranty"),
-		},
-	}
-	if bytes, err := msg.Marshal(nil); err != nil {
-		panic(err)
-	} else {
-		return bytes
-	}
-}
-
-func (p *Pinger) SendPing(payload []byte) {
-	newPing := registry.Ping{
-		PingerID:      p.ID,
-		SourceIP:      p.SourceIP,
-		DestinationIP: p.DestinationIP,
-		Sequence:      p.Sequence,
-		SentAt:        time.Now(),
-		ReceivedAt:    nil,
-	}
-	if _, err := p.Connection.WriteTo(
-		payload,
-		&net.IPAddr{
-			IP: net.ParseIP(p.DestinationIP),
-		}); err != nil {
-		panic(err)
-	}
-	registry.SentPings <- newPing
 }
